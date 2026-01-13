92c08cb44d0016b2e4697635dfdc4aad7c332e0d
Won Lee <wonseob2207@gmail.com>
2026-01-12 23:43:58 -0600

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/exercise.js
@@ -0,0 +1,32 @@
+// JavaScript Fundamentals - Part 1
+// Values and Variables
+let country = 'South Korea';
+let contient = 'Asia';
+let population = 50;
+
+console.log(country);
+console.log(contient);
+console.log(population);
+
+// Data Types
+const isIsland = false;
+let language = 'English';
+
+console.log(typeof isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// Basic Operators
+let newPopulation = population / 2;
+console.log(newPopulation ++);
+
+let finalndPopulation = 6;
+let averagePopulation = 33;
+
+console.log(population > populationFinland);
+console.log(population > averagePopulation);
+
+let description1 = country + ' is in ' + continent + ', and its ' + population + ' milliion people speak ' + language;
+
+console.log(description1);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+const age = 17;
+const isOldEnough = age >= 18;
+
+if(age >= 18) {
+    console.log('Sarah can start her driving license ✅')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah too young. Wait another ${yearsLeft} years ❌`)
+}
+
+const birthYear = 1991;
+
+const helloWorld = "1"
